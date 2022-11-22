a824b41b4e8195c582e762a18382db2e20e358a7
Sergio Gomez <sergio.gomez@eynes.com.ar>
2022-11-21 21:32:44 -0300

    [ADD] model for customers

--- apps/accounting_records/models.py
+++ apps/accounting_records/models.py
@@ -41,7 +41,7 @@ class Account (models.Model):
 
 class Category (MPTTModel):
     '''Category model for save the categories of the expense and income'''
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) 
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, null=True, blank=True)
     parent = TreeForeignKey ('self', on_delete=models.CASCADE, related_name='children', 
@@ -71,7 +71,7 @@ class Category (MPTTModel):
 class MethodOfPayment (models.Model):
     '''Method of payment model for save the methods of payment of the user'''
     
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)
     name = models.CharField(max_length=255)
     class Meta:
         verbose_name_plural="Forma de pagos"
@@ -94,7 +94,8 @@ class Records(models.Model):
     payment_date = models.DateField(default=date.today)
     category_id = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
     account_id = models.ForeignKey('Account', on_delete=models.SET_NULL, null=True)
-    method_of_payment_id = models.ForeignKey('MethodOfPayment', on_delete=models.CASCADE, null=True, blank=True)    
+    method_of_payment_id = models.ForeignKey('MethodOfPayment', on_delete=models.CASCADE, null=True, blank=True)
+    customer_id = models.ForeignKey('Customer', on_delete=models.SET_NULL, null=True, blank=True)
     voucher = models.FileField(upload_to='vouchers/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -147,3 +148,48 @@ class Records(models.Model):
     @property
     def get_comprobante(self):
         return self.voucher.url
+
+class Customer (models.Model):
+    '''Customer model for save the customers of the user'''
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='customers/img/', null=True, blank=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    website = models.URLField(max_length=255, null=True, blank=True)
+    cuil_cuit = models.CharField(max_length=255, null=True, blank=True)
+    class Ivatype (models.TextChoices):
+        IVARESPONSABLEINSCRIPTO = '1', _('IVA Responsable Inscripto')
+        IVARESPONSABLENOINSCRIPTO = '2', _('IVA Responsable No Inscripto')
+        IVANORESPONSABLE = '3', _('IVA No Responsable')
+        IVASUJETOEXENTO = '4', _('IVA Sujeto Exento')
+        CONSUMIDORFINAL = '5', _('Consumidor Final')
+        RESPONSABLEMONOTRIBUTO = '6', _('Responsable Monotributo')
+        SUJETONOCATEGORIZADO = '7', _('Sujeto No Categorizado')
+        PROVEEDORDELEXTERIOR = '8', _('Proveedor del Exterior')
+        CLIENTEDELEXTERIOR = '9', _('Cliente del Exterior')
+        IVALIBERADO = '10', _('IVA Liberado - Ley Nº 19.640')
+        IVARESPONSABLEINSCRPTO_AGENTEDEPERCEPCION = '11', _('IVA Responsable Inscripto - Agente de Percepción')
+        PEQUENIOCONTRIBUYENTEEVENTUAL = '12', _('Pequeño Contribuyente Eventual')
+        MONOTRIBUTISTASOCIAL = '13', _('Monotributista Social')
+        PEQUENIOCONTRIBUYENTESOCIAL_EVENTUAL = '14', _('Pequeño Contribuyente Social Eventual')
+        IVANOALCANZADO = '15', _('IVA No Alcanzado')
+        MONOTRIBUTISTATRABAJADORINDEPENDIENTEPROMOVIDO = '16', _('Monotributista Trabajador Independiente Promovido')
+    ivatype = models.CharField(max_length=2, choices=Ivatype.choices, default=Ivatype.IVARESPONSABLEINSCRIPTO, null=True, blank=True)
+    class CustomerType (models.TextChoices):
+        CLIENTEA = 'A', _('Cliente A - Empresa Grande')
+        CLIENTEB = 'B', _('Cliente B - Pyme')
+        CLIENTEC = 'C', _('Cliente C - Particular')
+    customertype = models.CharField(max_length=1, choices=CustomerType.choices, default=CustomerType.CLIENTEA, null=True, blank=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural="Clientes"
+        verbose_name="Cliente"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
